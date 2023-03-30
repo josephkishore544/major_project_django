40534df295d2cdb0abda2cac09eda09d61c517b2
Joseph Kishore <jeevankishore544@gmail.com>
2023-03-31 00:58:42 +0530

    resolved errors

--- project_app/views.py
+++ project_app/views.py
@@ -23,7 +23,8 @@ def generate_result(request) :
         print("Image generated")
     else :
         print("Something went wrong")
-    return render(request, 'generate_result.html')
+    image_url = "/media/generated.jpg"
+    return render(request, 'generate_result.html', {"image_url" : image_url})
 
 def manipulate_home(request) :
     mode = 'man'
@@ -36,7 +37,8 @@ def manipulate_last(request) :
     input_text = request.POST.get('text_input')
     my_handler.set_image_input_mode(image_input_mode)
     my_handler.set_text(input_text)
-    return render(request, 'manipulate_last.html')
+    image_url = "/media/generated.jpg"
+    return render(request, 'manipulate_last.html',{"image_url" : image_url})
 
 def manipulate_upload(request) :
     image_input_mode = 'upload'
@@ -50,17 +52,21 @@ def manipulate_result(request) :
     # would be called by POST method along with image 
     # so we need to save the image first before manipulation
     if request.method == "POST" :
-      uploaded_image = request.FILES['uploaded_image']
-      project_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-      file_path = "project/test/input.jpg"
-      upload_file_name = os.path.join(project_dir_path, file_path)
-      if os.path.exists(upload_file_name) :
-         os.remove(upload_file_name)
-      fs = FileSystemStorage()
-      name = fs.save('input.jpg', uploaded_image)
+        uploaded_image = request.FILES['uploaded_image']
+        project_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        file_path = "project/test/input.jpg"
+        upload_file_name = os.path.join(project_dir_path, file_path)
+        if os.path.exists(upload_file_name) :
+            os.remove(upload_file_name)
+        fs = FileSystemStorage()
+        name = fs.save('input.jpg', uploaded_image)
+        input_url = "/media/input.jpg"
+    else :
+        input_url = "/media/generated.jpg"
+    output_url = "/media/manipulated.jpg"
     success = my_handler.execute()
     if success :
         print("Image generated")
     else :
         print("Something went wrong")
-    return render(request, 'manipulate_result.html')
+    return render(request, 'manipulate_result.html',{"input_url" : input_url, "output_url" : output_url})
